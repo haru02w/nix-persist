cc846bd5edf765fb597f215ce815eaa1b448b269
Haru02w <haru02w@protonmail.com>
2024-09-14 21:48:35 -0300

    it doesn't work. PAM needs a patch and i won't happen soon

--- modules/nixos/essential.nix
+++ modules/nixos/essential.nix
@@ -52,7 +52,7 @@ in {
           mode = "0777";
         }
       ];
-      files = ["/etc/passwd" "/etc/shadow" "/etc/machine-id" "/etc/adjtime"];
+      files = ["/etc/machine-id" "/etc/adjtime"];
     };
   };
 }
